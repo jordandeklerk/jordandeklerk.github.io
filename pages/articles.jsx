2bb1c7515db0bcc0e20f5dafce3ac6db8a0c255b
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2024-10-01 21:31:46 -0400

    fix article descriptions

--- pages/articles.jsx
+++ pages/articles.jsx
@@ -5,28 +5,28 @@ const articles = [
   {
     id: 1,
     title: "An Introduction to Reinforcement Learning",
-    description: "This article introduces the fundamentals of reinforcement learning, covering key concepts like Markov Decision Processes, policies, and value functions. It also explains dynamic programming techniques for solving RL problems, serving as a guide for understanding RL theory and applications.",
+    description: "This post introduces the fundamentals of reinforcement learning from a theoretical perspective, covering key concepts like Markov Decision Processes, policies, and value functions. We also explain dynamic programming techniques for solving RL problems.",
     url: "https://ml-tutorials.netlify.app/blog/rl-intro/",
     image: "/blog-images/rl2.png"
   },
   {
     id: 2,
     title: "Masked Token Learning for Inpatient Diagnosis and Procedure Prediction",
-    description: "This article discusses a BERT-style transformer model adapted for predicting diagnoses and procedures from Electronic Health Records (EHR). It highlights the model's training process and effectiveness in improving predictive accuracy using a masked language model.",
+    description: "This post discusses a BERT-style transformer model adapted for predicting diagnoses and procedure codes from synthetic Electronic Health Records (EHR). We walkthrough the code step by step and provide intuition for the model and downstream applications.",
     url: "https://ml-tutorials.netlify.app/blog/ehr-bert/",
     image: "/blog-images/bert.jpg"
   },
   {
     id: 3,
     title: "Closing the Amortization Gap in Bayesian Deep Generative Models",
-    description: "This article examines amortized variational inference (A-VI) in Bayesian deep generative models, focusing on reducing the amortization gap in VAEs. It compares A-VI with traditional methods, showing its computational efficiency and competitive performance on benchmark datasets.",
+    description: "This post examines amortized variational inference (A-VI) in Bayesian deep generative models, focusing on reducing the amortization gap in VAEs. We compare A-VI with traditional methods, showing its computational efficiency and competitive performance on benchmark datasets.",
     url: "https://ml-tutorials.netlify.app/blog/amortized-bayes/",
     image: "/blog-images/vae.png"
   },
   {
     id: 4,
     title: "Fine-Tuning a Coding LLM for SQL Code Generation",
-    description: "This article explores fine-tuning an LLM for SQL code generation using QLoRA+, optimizing the model for text-to-SQL tasks. It covers techniques like quantization and noisy embeddings to enhance performance on a single GPU, making it suitable for resource-constrained environments.",
+    description: "This post implements fine-tuning a coding LLM for SQL code generation using QLoRA+. We cover techniques like quantization and noisy embeddings to enhance performance on a single GPU, making it suitable for resource-constrained environments.",
     url: "https://ml-tutorials.netlify.app/blog/open-code/",
     image: "/blog-images/llm.png"
   },
